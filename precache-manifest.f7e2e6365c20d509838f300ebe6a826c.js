6876261dec908fa1c708ee4b30a51923f77d76ba
Lucille Ablett <lucilleablett@gmail.com>
2020-02-27 12:03:43 +0000

    Updates

---
+++ precache-manifest.f7e2e6365c20d509838f300ebe6a826c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7255e3a183f3a3f991c34c3065143d15",
+    "url": "/ballot-bots/index.html"
+  },
+  {
+    "revision": "0ddd11e49e53624cd506",
+    "url": "/ballot-bots/static/css/main.24cecc84.chunk.css"
+  },
+  {
+    "revision": "2ebec34da81f3bdd4585",
+    "url": "/ballot-bots/static/js/2.99c67a19.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ballot-bots/static/js/2.99c67a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ddd11e49e53624cd506",
+    "url": "/ballot-bots/static/js/main.4e5b202d.chunk.js"
+  },
+  {
+    "revision": "bf670c1eeb7c372b2413",
+    "url": "/ballot-bots/static/js/runtime-main.d5611ba2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ballot-bots/precache-manifest.f7e2e6365c20d509838f300ebe6a826c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ballot-bots/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
